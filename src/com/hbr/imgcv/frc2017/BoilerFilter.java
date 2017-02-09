ae46099be538da687149324456c748d0dc18bc73
HBR Programmers <HBR Programmers@10.13.16.94>
2017-02-08 19:37:09 -0500

    Set up Boiler Filter, temporarily copied old Filter

--- src/com/hbr/imgcv/frc2017/BoilerFilter.java
+++ src/com/hbr/imgcv/frc2017/BoilerFilter.java
@@ -1,17 +1,83 @@
 package com.hbr.imgcv.frc2017;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.opencv.core.Mat;
 
+import com.hbr.imgcv.PolygonCv;
+import com.hbr.imgcv.filters.ColorRange;
 import com.hbr.imgcv.filters.MatFilter;
+import com.hbr.imgcv.frc2016.TargetFilterConfig.Imgproc;
 import com.hbr.imgcv.utils.FovCalculator;
 
 public class BoilerFilter extends Filter implements MatFilter, BoilerFilterConfig{
 
 	private FovCalculator fov;
+	private double distance; //distance from camera to wall
+	
+	private final ColorRange colorRange = new ColorRange(ImageProcessing.COLOR_MIN, ImageProcessing.COLOR_MAX, true);
+	
+	public PolygonCv bestTarget;
+	
+	public BoilerFilter(){
+		fov = new FovCalculator(Camera.FOV_X_DEGREES, Camera.RESOLUTION_X_PIXELS, distance);
+	}
 	
 	@Override
 	public Mat process(Mat srcImage) {
-		return null;
+		
+		List<PolygonCv> targets  = new ArrayList<>();
+	     PolygonCv  bestTarget;
+	     
+		Mat outputImage = srcImage.clone();
+		
+		colorRange.process(outputImage);
+		
+		targets = findTargets(outputImage);
+		
+		if(targets.size() > 0) {
+        	bestTarget = findBestTarget(targets);
+        	        	
+        	if(networkTable != null) { 
+        		targetAnalysis(bestTarget); //no return as it simply writes data to netTables 
+        		networkTable.putNumber("FrameCount", frameCount++); 
+        		
+        	}
+		}
+		
+		return outputImage;
+	}
+		
+	public void targetAnalysis(PolygonCv target){
+		
+	}
+	
+	//copied from frc2016.TargetFilter
+	public void setColorRangeConfig(File configFile) {
+		try {
+			FileReader configFileReader = new FileReader(configFile);
+			BufferedReader  configFileBReader = new BufferedReader(configFileReader);
+			
+			for(int i = 0; i < Imgproc.COLOR_MAX.length; i++) {
+				Imgproc.COLOR_MAX[i] = Integer.parseInt(configFileBReader.readLine());
+				System.out.println("Read & Set " + Imgproc.COLOR_MAX[i]);
+				Imgproc.COLOR_MIN[i] = Integer.parseInt(configFileBReader.readLine());
+				System.out.println("Read & Set " + Imgproc.COLOR_MIN[i]);
+
+			}
+			
+			colorRange.setRanges(ImageProcessing.COLOR_MIN, ImageProcessing.COLOR_MAX, true);
+			
+			configFileReader.close();
+			configFileBReader.close();
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
 	}
 
 }
--- src/com/hbr/imgcv/frc2017/BoilerFilterConfig.java
+++ src/com/hbr/imgcv/frc2017/BoilerFilterConfig.java
@@ -2,4 +2,18 @@ package com.hbr.imgcv.frc2017;
 
 public interface BoilerFilterConfig {
 
+	interface Camera {
+		public final double FOV_X_DEGREES       = 67; 
+		public final double FOV_Y_DEGREES       = 51;
+		public final double FOV_X_RADIANS       = Math.toRadians(FOV_X_DEGREES);
+		public final double FOV_Y_RADIANS       = Math.toRadians(FOV_Y_DEGREES);
+		public final double RESOLUTION_X_PIXELS = 800;
+		public final double RESOLUTION_Y_PIXELS = 600;
+		
+	}
+	
+	interface ImageProcessing{
+		public final int[] COLOR_MIN = {60, 255, 120}; //TODO: these values are approximate and should be adjusted
+		public final int[] COLOR_MAX = {0, 180, 0}; //TODO: these values are approximate and should be adjusted
+	}
 }
